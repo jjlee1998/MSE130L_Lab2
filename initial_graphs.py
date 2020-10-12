361c3a7303cab7bec1b0a4fafc7de662362c9eec
Jonathan <jlee@cfs.energy>
2020-10-12 01:32:07 -0400

    fixed plotting issue that wiped out negative data, and added progress colorbars

--- initial_graphs.py
+++ initial_graphs.py
@@ -23,12 +23,19 @@ for filename in filenames:
     fig, axes = plt.subplots(nrows=2, ncols=1)
     ax = axes.ravel()
     df = pd.read_csv(filename, sep=sep, names=names)
-    df.plot(x='potential_V', y='current_mA', ax=ax[0])
-    df.plot(x='potential_V', y='current_mA', ax=ax[1], logy=True)
+    df['abs_current_mA'] = np.abs(df['current_mA'])
+    df['progress'] = np.linspace(0, 1, df.shape[0])
+    #df.plot(x='potential_V', y='current_mA', ax=ax[0])
+    df.plot.scatter(x='potential_V', y='current_mA', c='progress',
+            colormap=plt.get_cmap('viridis'), colorbar=True, ax=ax[0], s=1)
+    #df.plot(x='potential_V', y='abs_current_mA', ax=ax[1], logy=True)
+    df.plot.scatter(x='potential_V', y='abs_current_mA', c='progress',
+            colormap=plt.get_cmap('viridis'), colorbar=True, ax=ax[1],
+            logy=True, s=1)
 
     figname = filename[7:]
-    ax[0].set_title(f'{figname} - Raw')
-    ax[1].set_title(f'{figname} - Log')
+    ax[0].set_title(f'{figname} - Raw Data')
+    ax[1].set_title(f'{figname} - Absolute Log')
     fig.set_size_inches(12, 16)
     plt.savefig(fname=f'Graphs/Initial {figname}', format='png', dpi=100)
     #plt.show()
